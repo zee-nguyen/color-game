f1f75db0726d12f3de75f09a4de64ceeace652bf
Zi Nguyen <zinguyen@Zis-MacBook-Pro.local>
2017-12-20 11:18:32 -0500

    add logic for when user clicks on a square and add function for picking a random color

--- script.js
+++ script.js
@@ -9,19 +9,36 @@ var colors = [
 
 var squares = document.querySelectorAll(".square");
 
-var pickedColor = "rgb(0, 255, 255)";
+var pickedColor = pickColor();
 var pickedColorDisplay = document.getElementById("pickedColorDisplay");
+var messageDisplay = document.querySelector("#messageDisplay");
+
 pickedColorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
-    //add event listener to each square
     squares[i].addEventListener("click", function(){
        var clickedColor = this.style.backgroundColor; 
         if (clickedColor === pickedColor) {
-            alert ("yaaay! correct!");
+            messageDisplay.textContent = "Correct!";
+            //change all square to the clickedColor
+            changeColors(clickedColor);
         } else {
-            alert ("sorry, incorrect...");
+            messageDisplay.textContent = "Try Again";
+            this.style.backgroundColor = "#232323";
         }
     });
+}
+
+function changeColors(color) {
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    //get a random number
+    var random = Math.floor(Math.random() * colors.length);
+    //return picked color at random position
+    return colors[random];
 }
